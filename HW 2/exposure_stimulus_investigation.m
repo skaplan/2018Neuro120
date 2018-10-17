d29fd27e6f21a16631147ac3ee977bab31886dd1
Sam Kaplan <sam@rubinkaplan.com>
2018-10-16 20:50:51 -0400

    std avergae

--- HW 2/exposure_stimulus_investigation.m
+++ HW 2/exposure_stimulus_investigation.m
@@ -5,15 +5,37 @@ load exposure_stimulus_experiment.mat
 stimulus_start_times = 0:1/6:(60-1/6); % In seconds
 
 % histogram(spikes_single_unit, stimulus_start_times)
-F = cell(360,1)
-for trialCount = 1:360
+c = 100
+a = 5
+R = zeros(a,c)
+for trialCount = 1:a
+    
     start = stimulus_start_times(trialCount)
     eee = (start + 1/6)
     filtered = spikes_single_unit((start <= spikes_single_unit) & (eee > spikes_single_unit)) - start
-    disp(filtered)
-    F{trialCount} = transpose(filtered )
+    for i = 1:c
+       mu = ((1/6) / c) * i
+       R(trialCount,i) = sum(normpdf(filtered, mu, 5/1000))
+    end
+   
 end
-plotSpikeRaster(F)
+plot(mean(R))
+% disp(size(R))
+% disp(mean(R))
+
+% 
+% F = cell(360,1)
+% for trialCount = 1:360
+%     start = stimulus_start_times(trialCount)
+%     eee = (start + 1/6)
+%     filtered = spikes_single_unit((start <= spikes_single_unit) & (eee > spikes_single_unit)) - start
+%     disp(filtered)
+%     F{trialCount} = transpose(filtered )
+% end
+% plotSpikeRaster(F)
+
+
+
 % plot(F)
 % ylim([0 size(spikeMat, 1)+1]);
 
