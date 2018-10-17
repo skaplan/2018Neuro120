b94b94b1f835d846ce3d8fd47e4881be1124bde1
Sam Kaplan <sam@rubinkaplan.com>
2018-10-16 20:25:49 -0400

    bbb

--- HW 2/exposure_stimulus_investigation.m
+++ HW 2/exposure_stimulus_investigation.m
@@ -1,36 +1,19 @@
 clear all
 
 load exposure_stimulus_experiment.mat
-load exposure_stimulus_experiment.mat
 
 stimulus_start_times = 0:1/6:(60-1/6); % In seconds
 
 % histogram(spikes_single_unit, stimulus_start_times)
-F = zeros(100, 15)
-for trialCount = 1:10
+F = cell(360,1)
+for trialCount = 1:360
     start = stimulus_start_times(trialCount)
     eee = (start + 1/6)
-    filtered = spikes_single_unit((start <= spikes_single_unit) & (eee > spikes_single_unit))
-    mini = start:1/100:(eee - 1/100)
-    [N, edges] = histcounts(filtered, mini)
-    
-    for spikeCount = 1:length(190)
-        plot([N(spikeCount) N(spikeCount)], ...
-            [trialCount-0.4 trialCount+0.4], 'k');
-    end
-
-%     disp("ab")
-%     disp(size(N))
-%         disp(size(F(2)))
-% 
-    F(trialCount,:) = N
-%     for spikeCount = 1:length(spikePos)
-%         plot([spikePos(spikeCount) spikePos(spikeCount)], ...
-%             [trialCount-0.4 trialCount+0.4], 'k');
-%     end
+    filtered = spikes_single_unit((start <= spikes_single_unit) & (eee > spikes_single_unit)) - start
+    disp(filtered)
+    F{trialCount} = transpose(filtered )
 end
-disp(F)
-ylim([0 size(10, 1)+1]);
+plotSpikeRaster(F)
 % plot(F)
 % ylim([0 size(spikeMat, 1)+1]);
 
